d3036f645ca9eb3ed94ed5cdeea835a1d9910639
P Sai Dhruv <saidhruv03@gmail.com>
2021-04-11 16:00:39 +0530

    Minor Changes30

--- SU_Learn/settings.py
+++ SU_Learn/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from .secrets import host_password,secret_key,client_id,oauth_key
-import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -165,6 +164,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL= "/media/"
