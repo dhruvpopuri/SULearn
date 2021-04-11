354767b8e2a5b3b945a77a18c421fb2daa7ce88a
P Sai Dhruv <saidhruv03@gmail.com>
2021-04-11 16:47:27 +0530

    Minor Changes41

--- SU_Learn/settings.py
+++ SU_Learn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
